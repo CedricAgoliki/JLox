4e4b2ec711c158c0ae542ec02a0f8dce2e60fd36
Cédric Agoliki <agolikicedric@gmail.com>
2023-10-08 07:49:47 +0000

    Add an Expression generator

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -1,3 +1,8 @@
+package lox;
+
+import lox.Scanner;
+import lox.Token;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/lox/Token.java
+++ src/lox/Token.java
@@ -1,3 +1,5 @@
+package lox;
+
 enum TokenType {
     // single character tokens
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
